54bb7f5370e2578a63f812913a5ecb21a049ccd1
KPR <130840048+iampavelk@users.noreply.github.com>
2023-11-04 17:56:54 +0200

    refactoring of products api

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import uvicorn
 
 from items_views import router as items_router
 from users.views import router as users_router
+from api_v1 import router as router_v1
 
 from core.models import Base, db_helper
+from core.config import settings
 
 
 @asynccontextmanager
@@ -16,6 +18,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 app.include_router(items_router)
 app.include_router(users_router)
 
